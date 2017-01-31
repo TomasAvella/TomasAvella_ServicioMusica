90541b3df4ed0a28687c831aaa75cece8d73a512
Tomas Avella <tomasavella007@gmail.com>
2017-01-31 16:06:22 +0100

    Practica 8.3

--- app/src/main/java/org/example/serviciomusica/ServicioMusica.java
+++ app/src/main/java/org/example/serviciomusica/ServicioMusica.java
@@ -16,24 +16,38 @@ import android.widget.Toast;
 public class ServicioMusica extends Service {
 
     MediaPlayer reproductor;
-    //private NotificationManager notificationManager;
-   // private static final int ID_NOTIFICACION = 1;
+    private NotificationManager notificationManager;
+    private static final int ID_NOTIFICACION = 1;
 
 
     @Override
     public void onCreate() {
         Toast.makeText(this, "Servicio creado", Toast.LENGTH_SHORT).show();
         reproductor = MediaPlayer.create(this, R.raw.audio);
-        //notificationManager= (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notificationManager= (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
     }
-    /*@Override
+    @Override
     public int onStartCommand(Intent intent, int flags, int idArranque) {
         Toast.makeText(this,"Servicio arrancado "+ idArranque, Toast.LENGTH_SHORT).show();
 
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+        long vibrate[] = {0, 100, 200, 300};
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("Reproduciendo música")
+                .setContentText("Información adicional")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pendingIntent)
+                .setVibrate(vibrate)
+                .setLights(0xffff00ff,0,0)
+                .getNotification();
+        notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+        notificationManager.notify(ID_NOTIFICACION, notification);
+
         reproductor.start();
         return START_STICKY;
-    }*/
+    }
     //Practica 8.2
     @Override
     public void onStart(Intent intent, int startId) {
@@ -45,7 +59,7 @@ public class ServicioMusica extends Service {
     public void onDestroy() {
         Toast.makeText(this,"Servicio detenido", Toast.LENGTH_SHORT).show();
         reproductor.stop();
-        //notificationManager.cancel(ID_NOTIFICACION);
+        notificationManager.cancel(ID_NOTIFICACION);
     }
     @Override
     public IBinder onBind(Intent intent) {
